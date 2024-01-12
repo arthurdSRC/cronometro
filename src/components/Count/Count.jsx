829d8d8a187767f87d4a89ca70aa85d5d7947d50
arthur <arthur.souza.reis222@gmail.com>
2024-01-12 15:15:38 -0300

    funcionalidades de iniciar , pausar e reiniciar

--- src/components/Count/Count.jsx
+++ src/components/Count/Count.jsx
@@ -1,13 +1,83 @@
 
+import { useState, useEffect } from "react"
+
+
 import "./Count.css"
+import { useRef } from "react"
+
 
 const Count = () => {
+  const [action, setAction] = useState()
+
+  const secondsRef = useRef(0)
+  const minutesRef = useRef(0)
+  const hoursRef = useRef(0)
+  const interval = useRef()
+
+  const [secondsTime, setSecondsTime] = useState()
+  const [minutesTime, setMinutesTime] = useState()
+  const [hoursTime, setHoursTime] = useState()
+
+  const FormatTime = (time) => {
+    return time < 10 ? `0${time}` : time
+  }
+
+  const start = () => {
+    secondsRef.current++
+
+    if (secondsRef.current === 60) {
+      minutesRef.current++
+      secondsRef.current = 0;
+    }
+    if (minutesRef.current === 60) {
+      hoursRef.current++
+      minutesRef.current = 0;
+    }
+
+    setSecondsTime(FormatTime(secondsRef.current))
+    setMinutesTime(FormatTime(minutesRef.current))
+    setHoursTime(FormatTime(hoursRef.current))
+
+  }
+
+  const reset = () => {
+    clearInterval(interval.current)
+
+
+    secondsRef.current = 0
+    minutesRef.current = 0
+    hoursRef.current = 0
+    setSecondsTime(0)
+    setMinutesTime(0)
+    setHoursTime(0)
+
+
+  }
+
+
+
+
+  useEffect(() => {
+    if (action === "running") {
+      interval.current = setInterval(start, 1000)
+    }
+    if (action === "stop") {
+      clearInterval(interval.current)
+    }
+    if (action === "reset") {
+      reset();
+    }
+
+  }, [action])
 
 
   return (
-
     <div>
-      <h1>count</h1>
+      {`${hoursTime}:${minutesTime}:${secondsTime}`}
+      <button onClick={() => setAction("running")}>Começar</button>
+      <button onClick={() => setAction("stop")}>Parar</button>
+      <button onClick={() => setAction("reset")}>Parar</button>
+
     </div>
   )
 
